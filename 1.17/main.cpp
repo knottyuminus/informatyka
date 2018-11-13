7f6b50c764f9b598a29300b486dbaecbce5bcb38
Szymon Stelmach <thematosek@gmail.com>
2018-11-13 09:11:08 +0100

    Add files via upload
    STRONA 27
    ZADANIE 1.17

---
+++ 1.17/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{ 
+float x;
+cout << "Podaj rzeczywista z przedzialu -5 do 10 " << endl;
+cin >> x;
+
+
+	if (x<7){
+	cout << pow(x,3) + 1;
+	}
+	if (x=7){
+	cout << cos(x-1);
+	}
+	if(x=9){
+		cout << sqrt(3*x);
+	}
+	if (x<-5 || x>10){
+		cout << -8*x;
+	}	
+else
+{
+	cout << "Blad danych" << endl;
+}
+}
+
